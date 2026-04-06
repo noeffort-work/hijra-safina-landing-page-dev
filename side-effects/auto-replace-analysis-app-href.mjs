7db201e7eb300f51c30c90c3f98514e5775e1a96
Luthfi Solahudin <luthfisolahudin@outlook.com>
2026-04-06 11:40:08 +0700

    refactor: update app constant names

--- side-effects/auto-replace-analysis-app-href.mjs
+++ side-effects/auto-replace-analysis-app-href.mjs
@@ -1,13 +1,13 @@
-import { DEVELOPMENT_ANALYSIS_APP_URL, PRODUCTION_ANALYSIS_APP_URL } from "../config/analysis-app.mjs";
+import { DEVELOPMENT_APP_URL, PRODUCTION_APP_URL } from "../config/analysis-app.mjs";
 import { IS_PRODUCTION } from "../config/app.mjs";
 
 (function () {
     if (IS_PRODUCTION) return;
 
-    const elements = document.querySelectorAll(`[href="${PRODUCTION_ANALYSIS_APP_URL}"]`)
+    const elements = document.querySelectorAll(`[href="${PRODUCTION_APP_URL}"]`)
 
     elements.forEach((element) => {
-        element.setAttribute('href', DEVELOPMENT_ANALYSIS_APP_URL)
+        element.setAttribute('href', DEVELOPMENT_APP_URL)
     })
 
     console.debug(`Success replace href of ${elements.length} element(s)`)
